7b43d84f2df685703e27f96ed12021e92fd22cee
ridozaen <ridozaen@gmail.com>
2016-04-04 21:30:01 +0700

    Added files via upload
    file issota

---
+++ AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  ISSOTA TRAVEL
+//
+//  Created by irna fitriani on 3/19/16.
+//  Copyright © 2016 ARTE STUDIO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
++ (void)getInstance;
+
+@end
+
